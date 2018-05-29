89af98a41967788fe2e66c570fcd204806b6e31b
Jean-Philippe Levy <jeanphilippe.levy@gmail.com>
2018-05-29 15:03:13 +0200

    Fix notifier logs table creation

--- notifier/notifierconf.sh
+++ notifier/notifierconf.sh
@@ -21,7 +21,7 @@ MYSQL_ROOTUSER="root"
 MYSQL_ROOTPASSWORD="root66"
 MYSQL_NOTIFIERUSER="notifierSQL"
 MYSQL_NOTIFIERPASSWORD="Notifier66"
-DATABASE_STRUCT="/srv/eyesofnetwork/notifier-2.1.2/docs/db/notifier.sql"
+DATABASE_STRUCT="/srv/eyesofnetwork/notifier/docs/db/notifier.sql"
 mysql -h${SERVER} -P${PORT} -u${MYSQL_ROOTUSER} -p${MYSQL_ROOTPASSWORD} -e "CREATE DATABASE notifier;"
 mysql -h${SERVER} -P${PORT} -u${MYSQL_ROOTUSER} -p${MYSQL_ROOTPASSWORD} -D ${DATABASE} < ${DATABASE_STRUCT}
 mysql -h${SERVER} -P${PORT} -u${MYSQL_ROOTUSER} -p${MYSQL_ROOTPASSWORD} -e "GRANT ALL PRIVILEGES ON notifier.* TO '${MYSQL_NOTIFIERUSER}'@'localhost' IDENTIFIED BY '${MYSQL_NOTIFIERPASSWORD}';"
