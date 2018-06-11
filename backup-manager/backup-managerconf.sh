62f232a66fe5a0098a5acbc92664a5e81660be2b
Jean-Philippe Levy <jeanphilippe.levy@gmail.com>
2018-06-11 22:20:45 +0200

    Remove space in bdds list

--- backup-manager/backup-managerconf.sh
+++ backup-manager/backup-managerconf.sh
@@ -15,4 +15,4 @@ cat ${eonfile}  > ${conffile}
 
 # update databases list
 for i in `find /var/lib/mysql/* -type d |awk -F "/" '{print $5}' |grep -v test |grep -v performance_schema` ; do BDDS="$i $BDDS" ; done
-sed -i "s/^export BM_MYSQL_DATABASES*.*/export BM_MYSQL_DATABASES=\"$BDDS\"/g" ${conffile}
+sed -i "s/^export BM_MYSQL_DATABASES*.*/export BM_MYSQL_DATABASES=\"${BDDS%?}\"/g" ${conffile}
