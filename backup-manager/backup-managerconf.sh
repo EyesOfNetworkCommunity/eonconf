d934af955716fc185fb1002fcbaaf9c16cb165ce
Jean-Philippe Levy <jeanphilippe.levy@gmail.com>
2018-06-11 22:17:39 +0200

    Fix backup again

--- backup-manager/backup-managerconf.sh
+++ backup-manager/backup-managerconf.sh
@@ -1,10 +1,18 @@
 #!/bin/sh
 
+# backup-manager paths
 eonfile="/srv/eyesofnetworkconf/backup-manager/backup-manager.conf"
 conffile="/etc/backup-manager.conf"
 
+# update backup repository
+mkdir -p /var/archives &>/dev/null 
+mv /var/backup-manager/* /var/archives/ &>/dev/null
+rm -rf /var/backup-manager &>/dev/null
+
+# update backup config
 cp -arf ${conffile} ${conffile}.orig 
 cat ${eonfile}  > ${conffile} 
 
+# update databases list
 for i in `find /var/lib/mysql/* -type d |awk -F "/" '{print $5}' |grep -v test |grep -v performance_schema` ; do BDDS="$i $BDDS" ; done
 sed -i "s/^export BM_MYSQL_DATABASES*.*/export BM_MYSQL_DATABASES=\"$BDDS\"/g" ${conffile}
